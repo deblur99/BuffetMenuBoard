c37aee3a0b4eae9860923d927a64857609be43f6
deblur99 <deblur99@kakao.com>
2023-08-18 13:25:51 +0900

    Initial Commit

---
+++ BuffetMenuBoard/BuffetMenuBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuffetMenuBoardApp.swift
+//  BuffetMenuBoard
+//
+//  Created by 한현민 on 2023/08/18.
+//
+
+import SwiftUI
+
+@main
+struct BuffetMenuBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
